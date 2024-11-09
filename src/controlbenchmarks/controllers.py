1b7e92e3d5d6273f34a5989ad41ad1076cc5055d
Shengjie Xu <shengjiex98@gmail.com>
2024-11-09 13:21:29 -0500

    Renames

--- src/controlbenchmarks/controllers.py
+++ src/controlbenchmarks/controllers.py
@@ -24,57 +24,41 @@ class PolePlacement(AbstractController):
     def __init__(self, sys: ctrl.StateSpace, h: float, pole=0.9):
         """Design a discrete-time pole-placement controller for a system with a one-period control delay."""
         super().__init__(sys, h)
-        p, q = sysd_delay.B.shape
-        sysd_delay = ctrl.sample_system(sys, h, method="foh")
+        p, q = dsys_delay.B.shape
+        dsys_delay = ctrl.sample_system(sys, h, method="foh")
         p_vec = np.concatenate(([0], np.full(p, pole)))
-        self.K = ctrl.place(sysd_delay.A, sysd_delay.B, p_vec)
+        self.K = ctrl.place(dsys_delay.A, dsys_delay.B, p_vec)
 
     def __call__(self, x: np.ndarray, t: float) -> np.ndarray:
         return self.K @ x
-    
-def augment(sysd: ctrl.StateSpace):
-    """Augment a discrete-time state-space model with a one-period control delay."""
-    p = sysd.A.shape[0]
-    q = sysd.B.shape[1]
-    r = sysd.C.shape[0]
-    A = np.block([[sysd.A, sysd.B], [np.zeros((q, p + q))]])
-    B = np.block([[np.zeros((p, q))], [np.eye(q)]])
-    C = np.block([sysd.C, np.zeros((r, q))])
-    D = sysd.D
-    Ts = sysd.dt
-    return ctrl.StateSpace(A, B, C, D, Ts)
 
-def delay_lqr(sys, h, Q=None, R=None):
-    """Design a discrete-time LQR controller for a system with a one-period control delay."""
-    p, q = sys.B.shape
-    sysd_delay = augment(ctrl.c2d(sys, h, method="foh"))
+def delay_lqr(dsys_augmented: ctrl.StateSpace, Q: np.ndarray = None, R: np.ndarray = None):
+    """Design a discrete-time LQR controller for an augmented system with a one-period control delay."""
+    p = dsys_augmented.nstates - dsys_augmented.ninputs
+    q = dsys_augmented.ninputs
     if Q is None:
-        # Penalize the state only, not the previous control input
-        Q = np.block([[np.eye(p, p + q)], [np.zeros(q, p + q)]])
+        Q = np.eye(p)
     if R is None:
-        # Penalize the current control input
         R = np.eye(q)
-    K, _, _ = ctrl.lqr(sysd_delay, Q, R)
+    Q_augmented = np.block([[Q, np.zeros((p, q))], [np.zeros((q, p + q))]])
+    K, _, _ = ctrl.lqr(dsys_augmented, np.eye(p+q), R)
     return K
 
-
-def pole_place(sys, h, p=0.9):
+def pole_place(dsys_augmented: ctrl.StateSpace, pole: float = 0.9):
     """Design a discrete-time pole-placement controller for a system with a one-period control delay."""
-    sysd_delay = ctrl.sample_system(sys, h, method="foh")
-    n = sysd_delay.A.shape[0]
-    p_vec = np.concatenate(([0], np.full(n, p)))
-    K = ctrl.place(sysd_delay.A, sysd_delay.B, p_vec)
+    p, q = dsys_augmented.nstates - dsys_augmented.ninputs, dsys_augmented.ninputs
+    p_vec = np.concatenate((np.zeros(q), np.full(p, pole)))
+    K = ctrl.place(dsys_augmented.A, dsys_augmented.B, p_vec)
     return K
 
-
-def augment(sysd: ctrl.StateSpace):
+def augment(dsys: ctrl.StateSpace):
     """Augment a discrete-time state-space model with a one-period control delay."""
-    p = sysd.A.shape[0]
-    q = sysd.B.shape[1]
-    r = sysd.C.shape[0]
-    A = np.block([[sysd.A, sysd.B], [np.zeros((q, p + q))]])
+    p = dsys.A.shape[0]
+    q = dsys.B.shape[1]
+    r = dsys.C.shape[0]
+    A = np.block([[dsys.A, dsys.B], [np.zeros((q, p + q))]])
     B = np.block([[np.zeros((p, q))], [np.eye(q)]])
-    C = np.block([sysd.C, np.zeros((r, q))])
-    D = sysd.D
-    Ts = sysd.dt
+    C = np.block([dsys.C, np.zeros((r, q))])
+    D = dsys.D
+    Ts = dsys.dt
     return ctrl.StateSpace(A, B, C, D, Ts)
--- src/controlbenchmarks/models.py
+++ src/controlbenchmarks/models.py
@@ -13,7 +13,7 @@ B_rc = np.array([[1/(r_1*c_1)], [0]])
 C_rc = np.eye(2)
 D_rc = np.zeros((2, 1))
 
-sys_rc = ctrl.StateSpace(A_rc, B_rc, C_rc, D_rc)
+_sys_rc = ctrl.StateSpace(A_rc, B_rc, C_rc, D_rc)
 
 # F1-tenth car
 v = 6.5
@@ -23,7 +23,7 @@ B_f1 = np.array([[0], [v/L]])
 C_f1 = np.array([[1, 0]])
 D_f1 = np.zeros((1, 1))
 
-sys_f1 = ctrl.StateSpace(A_f1, B_f1, C_f1, D_f1)
+_sys_f1 = ctrl.StateSpace(A_f1, B_f1, C_f1, D_f1)
 
 # DC motor
 A_dc = np.array([[-10, 1], [-0.02, -2]])
@@ -31,7 +31,7 @@ B_dc = np.array([[0], [2]])
 C_dc = np.array([[1, 0]])
 D_dc = np.zeros((1, 1))
 
-sys_dc = ctrl.StateSpace(A_dc, B_dc, C_dc, D_dc)
+_sys_dc = ctrl.StateSpace(A_dc, B_dc, C_dc, D_dc)
 
 # Car suspension system
 A_cs = np.array([[0., 1., 0., 0.],
@@ -42,7 +42,7 @@ B_cs = np.array([[0.], [80.], [20.], [-1120.]])
 C_cs = np.array([[1, 0, 0, 0]])
 D_cs = np.zeros((1, 1))
 
-sys_cs = ctrl.StateSpace(A_cs, B_cs, C_cs, D_cs)
+_sys_cs = ctrl.StateSpace(A_cs, B_cs, C_cs, D_cs)
 
 # Electronic wedge brake
 A_ew = np.array([[0, 1], [8.3951e3, 0]])
@@ -50,7 +50,7 @@ B_ew = np.array([[0], [4.0451]])
 C_ew = np.array([[7.9920e3, 0]])
 D_ew = np.zeros((1, 1))
 
-sys_ew = ctrl.StateSpace(A_ew, B_ew, C_ew, D_ew)
+_sys_ew = ctrl.StateSpace(A_ew, B_ew, C_ew, D_ew)
 
 # Cruise control 1
 A_c1 = np.array([[-0.05]])
@@ -58,7 +58,7 @@ B_c1 = np.array([[0.01]])
 C_c1 = np.array([[1]])
 D_c1 = np.zeros((1, 1))
 
-sys_c1 = ctrl.StateSpace(A_c1, B_c1, C_c1, D_c1)
+_sys_c1 = ctrl.StateSpace(A_c1, B_c1, C_c1, D_c1)
 
 # Cruise control 2
 A_cc = np.array([[0, 1, 0], [0, 0, 1], [-6.0476, -5.2856, -0.238]])
@@ -66,15 +66,15 @@ B_cc = np.array([[0], [0], [2.4767]])
 C_cc = np.array([[1, 0, 0]])
 D_cc = np.zeros((1, 1))
 
-sys_cc = ctrl.StateSpace(A_cc, B_cc, C_cc, D_cc)
+_sys_cc = ctrl.StateSpace(A_cc, B_cc, C_cc, D_cc)
 
 # Dictionary of systems
 sys_variables = {
-    'RC': sys_rc,
-    'F1': sys_f1,
-    'DC': sys_dc,
-    'CS': sys_cs,
-    'EW': sys_ew,
-    'C1': sys_c1,
-    'CC': sys_cc
+    'RC': _sys_rc,
+    'F1': _sys_f1,
+    'DC': _sys_dc,
+    'CS': _sys_cs,
+    'EW': _sys_ew,
+    'C1': _sys_c1,
+    'CC': _sys_cc
 }
