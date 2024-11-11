6f971a2063364a9a488355527fcb8f4230f8d886
Shengjie Xu <shengjiex98@gmail.com>
2024-11-10 21:12:42 -0500

    Remove class syntax

--- src/controlbenchmarks/controllers.py
+++ src/controlbenchmarks/controllers.py
@@ -1,51 +1,20 @@
 import numpy as np
 import control as ctrl
-from typing import Callable
-
-
-class AbstractController():
-    def __init__(self, sys, h):
-        self.sys = sys
-        self.h = h
-
-    def __call__(self, x, t):
-        raise NotImplementedError
-    
-class DelayLQR(AbstractController):
-    def __init__(self, sys: ctrl.StateSpace, h: float, Q=None, R=None):
-        """Design a discrete-time LQR controller for a system with a one-period control delay."""
-        super().__init__(sys, h)
-        self.K = delay_lqr(sys, h, Q, R)
-
-    def __call__(self, x: np.ndarray, t: float) -> np.ndarray:
-        return self.K @ x
-    
-class PolePlacement(AbstractController):
-    def __init__(self, sys: ctrl.StateSpace, h: float, pole=0.9):
-        """Design a discrete-time pole-placement controller for a system with a one-period control delay."""
-        super().__init__(sys, h)
-        p, q = dsys_delay.B.shape
-        dsys_delay = ctrl.sample_system(sys, h, method="foh")
-        p_vec = np.concatenate(([0], np.full(p, pole)))
-        self.K = ctrl.place(dsys_delay.A, dsys_delay.B, p_vec)
-
-    def __call__(self, x: np.ndarray, t: float) -> np.ndarray:
-        return self.K @ x
 
 def delay_lqr(dsys_augmented: ctrl.StateSpace, Q: np.ndarray = None, R: np.ndarray = None):
     """Design a discrete-time LQR controller for an augmented system with a one-period control delay."""
     p = dsys_augmented.nstates - dsys_augmented.ninputs
     q = dsys_augmented.ninputs
     if Q is None:
-        Q = np.eye(p)
+        Q = np.pad(np.eye(p), (0, q))
     if R is None:
         R = np.eye(q)
-    Q_augmented = np.block([[Q, np.zeros((p, q))], [np.zeros((q, p + q))]])
-    K, _, _ = ctrl.lqr(dsys_augmented, np.eye(p+q), R)
+    K, _, _ = ctrl.lqr(dsys_augmented, Q, R)
     return K
 
 def pole_place(dsys_augmented: ctrl.StateSpace, pole: float = 0.9):
-    """Design a discrete-time pole-placement controller for a system with a one-period control delay."""
+    """Design a discrete-time pole-placement controller for a system with a one-period control delay.
+    (need further analysis for setting the pole vector)"""
     p, q = dsys_augmented.nstates - dsys_augmented.ninputs, dsys_augmented.ninputs
     p_vec = np.concatenate((np.zeros(q), np.full(p, pole)))
     K = ctrl.place(dsys_augmented.A, dsys_augmented.B, p_vec)
